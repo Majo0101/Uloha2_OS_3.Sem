2273c32ed4514e4bc50da69d9f9da07c4cbde66c
Pondelok <bodnar.marian@gmail.com>
2022-11-30 22:19:43 +0100

    Data update

--- data.py
+++ data.py
@@ -2,6 +2,40 @@ class Node:
     def __init__(self, in_order):
         self.__file_name = "--------"
         self.__in_order = in_order
+        self.__bin = False
+        self.__address_data = []
+
+    def get_file_name(self):
+        return self.__file_name
+
+    def set_file_name(self, x):
+        self.__file_name = x
+
+    def get_in_order(self):
+        return self.__in_order
+
+    def set_in_order(self, x):
+        self.__in_order = x
+
+    def get_bin(self):
+        return self.__bin
+
+    def set_bin(self, x):
+        self.__bin = x
+
+    def get_address_data(self):
+        return self.__address_data
+
+    def set_address_data(self, x):
+        self.__address_data.append(x)
+
+    def del_address_data(self):
+        self.__address_data.clear()
+
+    node_name = property(get_file_name, set_file_name)
+    node_order = property(get_in_order, set_in_order)
+    node_bin = property(get_bin, set_bin)
+    node_register = property(get_address_data, set_address_data, del_address_data)
 
     def __str__(self):
         return "I am Node"
@@ -24,8 +58,8 @@ class DataBlock:
     def set_data_block(self, x):
         self.__data_block = x
 
-    in_use = property(get_used_block, set_used_block)
-    io_data = property(get_data_block, set_data_block)
+    data_use = property(get_used_block, set_used_block)
+    data_data = property(get_data_block, set_data_block)
 
     def __str__(self):
         return "I am a Data block"
